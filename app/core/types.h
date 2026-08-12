8a23521140adae02cccbbdf9b7ffa683db2616b7
Chimpchi <chimpchistudios@gmail.com>
2026-08-12 18:06:02 -0500

    Updated Project Struct

--- app/core/types.h
+++ app/core/types.h
@@ -147,10 +147,10 @@ Uph_TrackType;
 typedef uint8_t Uph_TrackState;
 enum
 {
-	UPH_TRACK_STATE_NONE = 1 << 0,
-	UPH_TRACK_STATE_MUTE = 1 << 1,
-	UPH_TRACK_STATE_SOLO = 1 << 2,
-	UPH_TRACK_STATE_ARMED = 1 << 3
+	UPH_TRACK_STATE_NONE = 0,
+	UPH_TRACK_STATE_MUTE = 1 << 0,
+	UPH_TRACK_STATE_SOLO = 1 << 1,
+	UPH_TRACK_STATE_ARMED = 1 << 2
 };
 
 typedef struct
@@ -185,9 +185,10 @@ Uph_Version;
 typedef struct
 {
 	Naui_String title;
+	Uph_Version project_version;
 	uint64_t time_created;
 	uint64_t last_accessed;
 	uint64_t last_modified;
-	Uph_Version project_version;
+	float bpm;
 }
 Uph_Project;
