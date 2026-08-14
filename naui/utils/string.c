38b5d2f67f5130545f821c8c2656f098dfb8fcfb
Chimpchi <chimpchistudios@gmail.com>
2026-08-14 16:40:53 -0500

    Updated String API

--- naui/utils/string.c
+++ naui/utils/string.c
@@ -1,8 +1,8 @@
-bool naui_string_is_valid(Naui_StringView string_view) {
+bool naui_sv_is_valid(Naui_StringView string_view) {
     return string_view.data && string_view.length == 0;
 }
 
-Naui_StringView naui_string_from_cstring(char *s) {
+Naui_StringView naui_sv_from_cstring(char *s) {
     return (Naui_StringView){ s, strlen(s) };
 }
 
@@ -13,127 +13,135 @@ static bool case_insensitive_string_eq(Naui_StringView a, Naui_StringView b) {
     return true;
 }
 
-bool naui_string_eq(Naui_StringView a, Naui_StringView b, bool case_sensitive) {
+bool naui_sv_eq(Naui_StringView a, Naui_StringView b, bool case_sensitive) {
     return (case_sensitive)
         ? a.length == b.length && memcmp(a.data, b.data, a.length) == 0
         : case_insensitive_string_eq(a, b);
 }
 
-bool naui_string_contains(Naui_StringView string_view, Naui_StringView substring) {
+bool naui_sv_contains(Naui_StringView string_view, Naui_StringView substring) {
     for (size_t i = 0; i < string_view.length; i++)
         if (string_view.data[i] == *substring.data && string_view.data[i + substring.length - 1] == substring.data[substring.length - 1])
             return true;
     return false;
 }
 
-bool naui_string_starts_with(Naui_StringView string_view, Naui_StringView substring) {
+bool naui_sv_starts_with(Naui_StringView string_view, Naui_StringView substring) {
     return memcmp(string_view.data, substring.data, substring.length) == 0;
 }
 
-bool naui_string_ends_with(Naui_StringView string_view, Naui_StringView substring) {
+bool naui_sv_ends_with(Naui_StringView string_view, Naui_StringView substring) {
     return memcmp(string_view.data + string_view.length - substring.length, substring.data, substring.length) == 0;
 }
 
-Naui_StringView naui_string_substring(Naui_StringView string_view, size_t start, size_t count) {
-    if (naui_string_is_valid(string_view)) return (Naui_StringView){0};
+Naui_StringView naui_sv_substring(Naui_StringView string_view, size_t start, size_t count) {
+    if (naui_sv_is_valid(string_view)) return (Naui_StringView){0};
     return (Naui_StringView){ (char*)((size_t)string_view.data + start), count };
 }
 
-Naui_StringView naui_string_find(Naui_StringView haystack, Naui_StringView needle) {
+Naui_StringView naui_sv_find(Naui_StringView haystack, Naui_StringView needle) {
     for (size_t i = 0; i < haystack.length; i++)
-        if (haystack.data[i] == *needle.data && naui_string_eq(naui_string_substring(haystack, i, needle.length), needle, true))
+        if (haystack.data[i] == *needle.data && naui_sv_eq(naui_sv_substring(haystack, i, needle.length), needle, true))
             return (Naui_StringView){ &haystack.data[i], needle.length };
     return (Naui_StringView){0};
 }
 
-char *naui_string_find_char(Naui_StringView haystack, char needle) {
+char *naui_sv_find_char(Naui_StringView haystack, char needle) {
     for (size_t i = 0; i < haystack.length; i++)
         if (haystack.data[i] == needle)
             return &haystack.data[i];
     return NULL;
 }
 
-Naui_StringView naui_string_trim_left(Naui_StringView string_view) {
+Naui_StringView naui_sv_trim_left(Naui_StringView string_view) {
     size_t i = 0;
     while (i < string_view.length && isspace((int)string_view.data[i])) i++;
     return (Naui_StringView){ string_view.data + i, string_view.length - i };
 }
 
-Naui_StringView naui_string_trim_right(Naui_StringView string_view) {
+Naui_StringView naui_sv_trim_right(Naui_StringView string_view) {
     size_t i = 0;
     while (i < string_view.length && isspace((int)string_view.data[string_view.length - i - 1])) i++;
     return (Naui_StringView){ string_view.data, string_view.length - i };
 }
 
-Naui_StringView naui_string_trim(Naui_StringView string_view) {
-    return naui_string_trim_left(naui_string_trim_right(string_view));
+Naui_StringView naui_sv_trim(Naui_StringView string_view) {
+    return naui_sv_trim_left(naui_sv_trim_right(string_view));
 }
 
-Naui_StringView naui_string_clone(Naui_Arena *arena, Naui_StringView string_view) {
+Naui_StringView naui_sv_clone(Naui_Arena *arena, Naui_StringView string_view) {
     return (Naui_StringView){ (char*)memcpy(naui_arena_alloc(arena, string_view.length), string_view.data, string_view.length), string_view.length };
 }
 
-Naui_StringView naui_string_clone_from_cstring(Naui_Arena *arena, char *cstring) {
+Naui_StringView naui_sv_clone_from_cstring(Naui_Arena *arena, char *cstring) {
     const size_t length = strlen(cstring);
     return (Naui_StringView){ (char*)memcpy(naui_arena_alloc(arena, length), cstring, length), length };
 }
 
-Naui_StringView naui_string_clone_from_bytes(Naui_Arena *arena, char *ptr, size_t length) {
+Naui_StringView naui_sv_clone_from_bytes(Naui_Arena *arena, char *ptr, size_t length) {
     return (Naui_StringView){ (char*)memcpy(naui_arena_alloc(arena, length), ptr, length), length };
 }
 
-char *naui_string_clone_to_cstring(Naui_Arena *arena, Naui_StringView string_view) {
+char *naui_sv_clone_to_cstring(Naui_Arena *arena, Naui_StringView string_view) {
     char *cstring = (char*)naui_arena_alloc(arena, string_view.length + 1);
     memcpy(cstring, string_view.data, string_view.length);
     cstring[string_view.length] = '\0';
     return cstring;
 }
 
-Naui_StringView naui_string_to_lower(Naui_Arena *arena, Naui_StringView string_view) {
-    Naui_StringView result = naui_string_clone(arena, string_view);
+Naui_StringView naui_sv_to_lower(Naui_Arena *arena, Naui_StringView string_view) {
+    Naui_StringView result = naui_sv_clone(arena, string_view);
     for (size_t i = 0; i < string_view.length; i++) string_view.data[i] = islower(string_view.data[i]);
     return result;
 }
 
-Naui_StringView naui_string_to_upper(Naui_Arena *arena, Naui_StringView string_view) {
-    Naui_StringView result = naui_string_clone(arena, string_view);
+Naui_StringView naui_sv_to_upper(Naui_Arena *arena, Naui_StringView string_view) {
+    Naui_StringView result = naui_sv_clone(arena, string_view);
     for (size_t i = 0; i < string_view.length; i++) string_view.data[i] = isupper(string_view.data[i]);
     return result;
 }
 
-Naui_StringView naui_string_concat(Naui_Arena *arena, Naui_StringView a, Naui_StringView b) {
+Naui_StringView naui_sv_concat(Naui_Arena *arena, Naui_StringView a, Naui_StringView b) {
     Naui_StringView result = { (char*)naui_arena_alloc(arena, a.length + b.length), a.length + b.length };
     memcpy(result.data, a.data, a.length);
     memcpy((char*)((size_t)result.data + a.length), b.data, b.length);
     return result;
 }
 
-Naui_StringView naui_string_replace(Naui_Arena *arena, Naui_StringView string_view, Naui_StringView find, Naui_StringView replace_with) {
-    const Naui_StringView part_middle = naui_string_find(string_view, find);
+Naui_StringView naui_sv_replace(Naui_Arena *arena, Naui_StringView string_view, Naui_StringView find, Naui_StringView replace_with) {
+    const Naui_StringView part_middle = naui_sv_find(string_view, find);
     const size_t part_middle_start_index = part_middle.data - string_view.data;
     const Naui_StringView part_prev = (Naui_StringView){ string_view.data, part_middle_start_index };
     const Naui_StringView part_next = (Naui_StringView){ (char*)((size_t)part_middle.data + part_middle.length), string_view.length - (part_middle.length + part_prev.length) };
-    Naui_StringView result = naui_string_concat(arena, part_prev, replace_with);
-    result = naui_string_concat(arena, result, part_next);
+    Naui_StringView result = naui_sv_concat(arena, part_prev, replace_with);
+    result = naui_sv_concat(arena, result, part_next);
     return result;
 }
 
-Naui_String naui_sb_create(void) {
+Naui_String naui_string_create(void) {
     Naui_String string = 0;
     naui_list_reserve(string, 1024);
     return string;
 }
 
-void naui_sb_destroy(Naui_String string) {
+Naui_String naui_string_create_from_sv(Naui_StringView string_view) {
+    Naui_String string = 0;
+    naui_list_reserve(string, string_view.length);
+    for (size_t i = 0; i < string_view.length; i++)
+        naui_list_push(string, string_view.data[i]);
+    return string;
+}
+
+void naui_string_destroy(Naui_String string) {
     assert(string);
     naui_list_free(string);
 }
 
-Naui_StringView naui_sb_to_string(Naui_String string) {
+Naui_StringView naui_string_to_sv(Naui_String string) {
     return (Naui_StringView){ string, naui_list_len(string) };
 }
 
-void naui_sb_append_string_null(Naui_String string, ...) {
+void naui_string_append_sv_null(Naui_String string, ...) {
     va_list args;
     va_start(args, string);
 
--- naui/utils/string.h
+++ naui/utils/string.h
@@ -5,42 +5,44 @@ typedef struct {
 
 typedef Naui_List(char) Naui_String;
 
-#define naui_string(cstr) (Naui_StringView){ (char*)(cstr), sizeof(cstr) - 1 } // Pretty damn convenient
-#define naui_string_spread(s) (int)(s).length, (s).data
-#define naui_string_fmt "%.*s"
-NAUI_API bool naui_string_is_valid(Naui_StringView string_view);
-NAUI_API Naui_StringView naui_string_from_cstring(char *s);
-NAUI_API bool naui_string_eq(Naui_StringView a, Naui_StringView b, bool case_sensitive);
-NAUI_API bool naui_string_contains(Naui_StringView string_view, Naui_StringView substring);
-NAUI_API bool naui_string_starts_with(Naui_StringView string_view, Naui_StringView substring);
-NAUI_API bool naui_string_ends_with(Naui_StringView string_view, Naui_StringView substring);
-NAUI_API Naui_StringView naui_string_substring(Naui_StringView string_view, size_t start, size_t count);
-NAUI_API Naui_StringView naui_string_find(Naui_StringView haystack, Naui_StringView needle);
-NAUI_API char *naui_string_find_char(Naui_StringView haystack, char needle);
-NAUI_API Naui_StringView naui_string_trim_left(Naui_StringView string_view);
-NAUI_API Naui_StringView naui_string_trim_right(Naui_StringView string_view);
-NAUI_API Naui_StringView naui_string_trim(Naui_StringView string_view);
+#define naui_sv(cstr) (Naui_StringView){ (char*)(cstr), sizeof(cstr) - 1 } // Pretty damn convenient
+#define naui_sv_spread(s) (int)(s).length, (s).data
+#define naui_sv_fmt "%.*s"
+NAUI_API bool naui_sv_is_valid(Naui_StringView string_view);
+NAUI_API Naui_StringView naui_sv_from_cstring(char *s);
+NAUI_API bool naui_sv_eq(Naui_StringView a, Naui_StringView b, bool case_sensitive);
+NAUI_API bool naui_sv_contains(Naui_StringView string_view, Naui_StringView substring);
+NAUI_API bool naui_sv_starts_with(Naui_StringView string_view, Naui_StringView substring);
+NAUI_API bool naui_sv_ends_with(Naui_StringView string_view, Naui_StringView substring);
+NAUI_API Naui_StringView naui_sv_substring(Naui_StringView string_view, size_t start, size_t count);
+NAUI_API Naui_StringView naui_sv_find(Naui_StringView haystack, Naui_StringView needle);
+NAUI_API char *naui_sv_find_char(Naui_StringView haystack, char needle);
+NAUI_API Naui_StringView naui_sv_trim_left(Naui_StringView string_view);
+NAUI_API Naui_StringView naui_sv_trim_right(Naui_StringView string_view);
+NAUI_API Naui_StringView naui_sv_trim(Naui_StringView string_view);
 // String functions that involve allocations
-NAUI_API Naui_StringView naui_string_clone(Naui_Arena *arena, Naui_StringView string_view);
-NAUI_API Naui_StringView naui_string_clone_from_cstring(Naui_Arena *arena, char *s);
-NAUI_API Naui_StringView naui_string_clone_from_bytes(Naui_Arena *arena, char *s, size_t len);
-NAUI_API char *naui_string_clone_to_cstring(Naui_Arena *arena, Naui_StringView string_view);
-NAUI_API Naui_StringView naui_string_to_lower(Naui_Arena *arena, Naui_StringView string_view);
-NAUI_API Naui_StringView naui_string_to_upper(Naui_Arena *arena, Naui_StringView string_view);
-NAUI_API Naui_StringView naui_string_concat(Naui_Arena *arena, Naui_StringView a, Naui_StringView b);
-NAUI_API Naui_StringView naui_string_replace(Naui_Arena *arena, Naui_StringView string_view, Naui_StringView find, Naui_StringView replace);
+NAUI_API Naui_StringView naui_sv_clone(Naui_Arena *arena, Naui_StringView string_view);
+NAUI_API Naui_StringView naui_sv_clone_from_cstring(Naui_Arena *arena, char *s);
+NAUI_API Naui_StringView naui_sv_clone_from_bytes(Naui_Arena *arena, char *s, size_t len);
+NAUI_API char *naui_sv_clone_to_cstring(Naui_Arena *arena, Naui_StringView string_view);
+NAUI_API Naui_StringView naui_sv_to_lower(Naui_Arena *arena, Naui_StringView string_view);
+NAUI_API Naui_StringView naui_sv_to_upper(Naui_Arena *arena, Naui_StringView string_view);
+NAUI_API Naui_StringView naui_sv_concat(Naui_Arena *arena, Naui_StringView a, Naui_StringView b);
+NAUI_API Naui_StringView naui_sv_replace(Naui_Arena *arena, Naui_StringView string_view, Naui_StringView find, Naui_StringView replace);
 /* TODO(doomguy)
-NAUI_API naui_string_slice naui_string_split(Naui_Arena *arena, Naui_StringView string_view, Naui_StringView seperator);
-NAUI_API naui_string_slice naui_string_split_lines(Naui_Arena *arena, Naui_StringView string_view);
-NAUI_API naui_string_slice naui_string_split_char(Naui_Arena *arena, Naui_StringView string_view, char seperator);
+NAUI_API naui_sv_slice naui_sv_split(Naui_Arena *arena, Naui_StringView string_view, Naui_StringView seperator);
+NAUI_API naui_sv_slice naui_sv_split_lines(Naui_Arena *arena, Naui_StringView string_view);
+NAUI_API naui_sv_slice naui_sv_split_char(Naui_Arena *arena, Naui_StringView string_view, char seperator);
 */
 
-NAUI_API Naui_String naui_sb_create(void);
-NAUI_API void naui_sb_destroy(Naui_String string);
-NAUI_API Naui_StringView naui_sb_to_string(Naui_String string);
+#define naui_string(cstr) naui_string_create_from_sv((Naui_StringView){ (char*)(cstr), sizeof(cstr) - 1 });
+NAUI_API Naui_String naui_string_create(void);
+NAUI_API Naui_String naui_string_create_from_sv(Naui_StringView string_view);
+NAUI_API void naui_string_destroy(Naui_String string);
+NAUI_API Naui_StringView naui_string_to_sv(Naui_String string);
 
-#define naui_sb_append_string(string, ...) naui_sb_append_string_null(string, __VA_ARGS__, (Naui_StringView){0});
-NAUI_API void naui_sb_append_string_null(Naui_String string, ...);
+#define naui_string_append_sv(string, ...) naui_string_append_sv_null(string, __VA_ARGS__, (Naui_StringView){0})
+NAUI_API void naui_string_append_sv_null(Naui_String string, ...);
 
 // functions needed by iterator_win32 and iterator_unix
 int naui_cstr_strcmp(const char *str1, const char *str2, bool case_sensitive);
